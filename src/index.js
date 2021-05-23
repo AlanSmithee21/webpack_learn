0b9e386a2564ba53cac4b8c74668e241d5cf95aa
AlanSmithee21 <55044533+AlanSmithee21@users.noreply.github.com>
2021-05-23 14:06:15 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import _ from 'lodash'
+// import { NAME, Nijou } from './utilities'
+// import * as utilities from './utilities'
+
+import Tiger from './utilities'
+
+console.log(Tiger.say());
+
+// console.log(Nijou(3));
+// console.log(NAME);
+// console.log(utilities.Nijou(3));
+// console.log(utilities.NAME);
 
 function component(){
     const element = document.createElement('div');
